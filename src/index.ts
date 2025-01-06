5abb17dd74f70c1b92d0e643ae626c1e4d841c32
1216301160 <1216301160@qq.com>
2025-01-06 23:03:56 +0800

    feat: 修改主文件

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@
 // // export { MultiCascader }
 // // export * from './components'
 // // module.exports = require('./components');
-// export * from './components'
 import EmptyTable from './components/emptyTable/EmptyTable'
 import MultiCascader from './components/multiCascader/MultiCascader'
 
